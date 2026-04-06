857b56d25c39eb3a626c014d0fc8db403fe11b2f
Zakhar Pochuvalov <94977621+sobselena@users.noreply.github.com>
2026-04-07 00:12:46 +0300

    feat: add input field component

---
+++ src/components/input-field/input-field.ts
@@ -0,0 +1,54 @@
+import { Component } from '../../utils/Component';
+import './input-field.scss';
+
+type InputFieldProperties = {
+  classes: string[];
+  placeholder?: string;
+  value?: string;
+  type?: string;
+  onKeyUp?: (event: Event) => void;
+};
+
+export class InputField extends Component {
+  private onKeyUp?: (event: Event) => void;
+  constructor({ classes, placeholder, value, type, onKeyUp }: InputFieldProperties) {
+    super({ tag: 'input', classes });
+    if (placeholder) {
+      this.setPlaceholder(placeholder);
+    }
+    if (type) {
+      this.setType(type);
+    }
+    if (value) {
+      this.setAttribute('value', value);
+    }
+    if (onKeyUp) {
+      this.addKeyUpEvent(onKeyUp);
+    }
+  }
+
+  getValue(): string {
+    return (this.getNode() as HTMLInputElement).value;
+  }
+
+  setDeffaultValue(): void {
+    (this.getNode() as HTMLInputElement).value = '';
+  }
+
+  setPlaceholder(placeholder: string) {
+    this.setAttribute('placeholder', placeholder);
+  }
+
+  addKeyUpEvent(callback: EventListener) {
+    this.onKeyUp = (event: Event) => callback(event);
+    this.addListener('keyup', this.onKeyUp);
+  }
+  deleteKeyUpEvent() {
+    if (this.onKeyUp) {
+      this.deleteListener('keyup', this.onKeyUp);
+    }
+  }
+  setType(type: string) {
+    this.setAttribute('type', type);
+  }
+}
